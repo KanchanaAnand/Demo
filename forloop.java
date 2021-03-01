eb51b237ad8c15be6ed596894901a8245b9ce4e5
KanchanaAnand <79839913+KanchanaAnand@users.noreply.github.com>
2021-03-01 22:53:19 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,15 @@
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		for(int i=0;i<=7;i++)
+		{
+			System.out.println(+i);
+			
+		}
+			
+	}
+
+}
